a4ba42621a87933b99b4b0048ea2edede1984577
Abdelrahman Sobhy <abdelrahman.raye7@gmail.com>
2021-03-15 22:37:10 +0200

    - piking numbers

--- test-playground.playground/Contents.swift
+++ test-playground.playground/Contents.swift
@@ -525,3 +525,27 @@ import Foundation
 //    return costs.min()!
 //}
 //formingMagicSquare(s: [[5, 3, 4], [1, 5, 8], [6, 4, 2]])
+
+//func pickingNumbers(a: [Int]) -> Int {
+//    var res = 0
+//    var dict = [Int:Int]()
+//    for int in a {
+//        dict[int] = dict[int] == nil ? 1 : dict[int]! + 1
+//    }
+//    let keys = Array(dict.sorted { $0.key < $1.key }.map { $0.key })
+//    let values = Array(dict.sorted { $0.key < $1.key }.map { $0.value })
+//    var prev = keys[0]
+//    res = max(res, values[0])
+//    for i in 1..<keys.count {
+//        res = max(res, values[i])
+//        let curr = keys[i]
+//        if abs(prev - curr) <= 1 {
+//            res = max(res, values[i - 1] + values[i])
+//        }
+//        prev = keys[i]
+//    }
+//    return res
+//}
+//
+//print(pickingNumbers(a: [4,6,5,3,3,1])) // ->  3
+
